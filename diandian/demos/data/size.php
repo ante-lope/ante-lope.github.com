0f5b45665c3c11fc0e14b6a092a5978494e10000
dingman081130 <dingman081130@gmail.com>
2013-10-23 00:02:51 +0800

    resize

---
+++ diandian/demos/data/size.php
@@ -0,0 +1,24 @@
+<?php
+
+$width = 192;
+
+$ret = array(
+    "total"  => 24,
+    "result" => array(),
+);
+
+for($i=1;$i<=24;$i++){
+
+    $arr = getimagesize("{$i}.jpeg");
+    $w = $arr[0];
+    $h = $arr[1];
+    $height = ($h/$w) * $width;
+    $path = "/diandian/demos/data/{$i}.jpeg";
+    $ret["result"][] = array(
+            "image" => $path,
+            "width" => $width,
+            "height"=> $height
+            );
+}
+
+print_r(json_encode($ret));
